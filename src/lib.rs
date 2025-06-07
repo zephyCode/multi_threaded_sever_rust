f51826ec0f400d65779880f61fea18071ca5b45c
zephyCode <utkarsh.pundeer2001@outlook.com>
2025-06-08 00:32:28 +0530

    graceful clean up and shutdown implemented as well the final commit

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@ use std::{
 
 pub struct ThreadPool {
     _workers: Vec<Worker>,
-    _sender: mpsc::Sender<Job>
+    _sender: Option<mpsc::Sender<Job>>
 }
 
 type Job = Box<dyn FnOnce() + Send + 'static>;
@@ -20,7 +20,10 @@ impl ThreadPool {
         for id in 0..size {
             _workers.push(Worker::new(id, Arc::clone(&_receiver)));
         }
-        ThreadPool { _workers, _sender }
+        ThreadPool {
+            _workers,
+            _sender: Some(_sender),
+        }
     }
 
     pub fn execute<F> (&self, _f: F)
@@ -28,7 +31,17 @@ impl ThreadPool {
         F: FnOnce() + Send + 'static,
     {
         let job = Box::new(_f);
-        self._sender.send(job).unwrap();
+        self._sender.as_ref().unwrap().send(job).unwrap();
+    }
+}
+
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+        drop(self._sender.take());
+        for worker in &mut self._workers.drain(..) {
+            println!("Shutting down worker {}", worker._id);
+            worker._thread.join().unwrap();
+        }
     }
 }
 
@@ -41,9 +54,17 @@ impl Worker {
     fn new(_id: usize, _reciever: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
         let _thread = thread::spawn(move || {
             loop {
-                let job = _reciever.lock().unwrap().recv().unwrap();
-                println!("Worker {_id} got a job; executing.");
-                job();
+                let message = _reciever.lock().unwrap().recv();
+                match message {
+                    Ok(job) => {
+                        println!("Worker {_id} got a job; executing.");
+                        job();
+                    }
+                    Err(_) => {
+                        println!("Worker {_id} disconnected; shutting down!");
+                        break;
+                    }
+                }
             }
         });
         Worker { _id, _thread }
--- src/main.rs
+++ src/main.rs
@@ -11,12 +11,13 @@ use multi_thread_server::ThreadPool;
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
     let pool = ThreadPool::new(4);
-    for stream in listener.incoming() {
+    for stream in listener.incoming().take(2) {
         let _stream = stream.unwrap();
         pool.execute(|| {
             handle_connection(_stream);
         });
     }
+    println!("Shutting down!")
 }
 
 fn handle_connection(mut stream: TcpStream) {
